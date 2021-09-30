f34640031e1036aee98e7b0234cd0749f72a8c5c
Tyler Nijmeh <tylernij@gmail.com>
2021-09-29 18:05:01 -0700

    Skeleton for number game
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- number_game.c
+++ number_game.c
@@ -5,3 +5,40 @@ default variable max is set
 function to guess
 Ask user for a number between 1 and 10
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void play() {
+
+}
+
+void change() {
+
+}
+
+int main() {
+    int mode;
+
+    printf("Press 1 to play a game\n");
+    printf("Press 2 to change the max number\n");
+    printf("Press 3 to quit\n");
+
+    scanf("%d", &mode);
+
+    switch (mode)
+    {
+    case 1:
+        play();
+        break;
+    case 2:
+        change();
+        break;
+    case 3:
+        exit(0);
+    
+    default:
+        printf("Bad command!");
+        break;
+    }
+}
