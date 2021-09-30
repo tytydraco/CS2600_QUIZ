adda6b8205cb0ae7a2c4ee7fc9c083b93dea8fb9
Tyler Nijmeh <tylernij@gmail.com>
2021-09-29 18:11:24 -0700

    Base game
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- number_game.c
+++ number_game.c
@@ -9,36 +9,57 @@ Ask user for a number between 1 and 10
 #include <stdio.h>
 #include <stdlib.h>
 
-void play() {
+int max_number = 10;
 
+void play() {
+    int number = rand() % max_number + 1;
+
+    while (1) {
+        int guess;
+
+        printf("Guess: ");
+        scanf("%d", &guess);
+
+        if (guess > number) {
+            printf("Too high!\n");
+        } else if (guess < number) {
+            printf("Too low!\n");
+        } else {
+            printf("Winner!\n");;
+            break;
+        }
+    }
 }
 
 void change() {
-
+    printf("New random number: ");
+    scanf("%d", &max_number);
 }
 
 int main() {
     int mode;
 
-    printf("Press 1 to play a game\n");
-    printf("Press 2 to change the max number\n");
-    printf("Press 3 to quit\n");
+    while (1) {
+        printf("Press 1 to play a game\n");
+        printf("Press 2 to change the max number\n");
+        printf("Press 3 to quit\n");
 
-    scanf("%d", &mode);
-
-    switch (mode)
-    {
-    case 1:
-        play();
-        break;
-    case 2:
-        change();
-        break;
-    case 3:
-        exit(0);
-    
-    default:
-        printf("Bad command!");
-        break;
+        scanf("%d", &mode);
+        
+        switch (mode)
+        {
+        case 1:
+            play();
+            break;
+        case 2:
+            change();
+            break;
+        case 3:
+            exit(0);
+        
+        default:
+            printf("Bad command!\n");
+            break;
+        }
     }
 }
