3b45d89475f1d2805f03b2593541f34ca431ce27
Tyler Nijmeh <tylernij@gmail.com>
2021-09-29 17:46:27 -0700

    Create outline for files
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ number_game.c
@@ -0,0 +1,7 @@
+/*
+ask for number to decide the game
+functions for each mode
+default variable max is set
+function to guess
+Ask user for a number between 1 and 10
+*/
---
+++ restaurant_bill.c
@@ -0,0 +1,7 @@
+/*
+Ask for tax %
+Ask for tip %
+Randomly choose meal cost
+constants for meal costs
+function to randomly pick one
+*/
