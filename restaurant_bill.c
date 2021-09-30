e0ff2d5a55713a057883b6d5233457c0bc090ad9
Tyler Nijmeh <tylernij@gmail.com>
2021-09-29 18:01:35 -0700

    Build restauraunt code
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- restaurant_bill.c
+++ restaurant_bill.c
@@ -5,3 +5,26 @@ Randomly choose meal cost
 constants for meal costs
 function to randomly pick one
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float bills[] = {9.95, 4.55, 13.25, 22.35};
+
+int main() {
+    int tax;
+    int tip;
+
+    printf("Enter tax percent: ");
+    scanf("%d", &tax);
+    
+    printf("Enter tip percent: ");
+    scanf("%d", &tip);
+
+    int meal_index = rand() % 4;
+    float meal_price = bills[meal_index];
+    printf("Random meal price: %f\n", meal_price);
+
+    printf("Tax: %f\n", ((float) tax/100) * meal_price);
+    printf("Tip: %f\n", ((float) tip/100) * meal_price);
+}
